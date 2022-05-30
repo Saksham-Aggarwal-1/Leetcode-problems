5d9a9e6972007bdcf1d43b2fbad2e55f204a03e3
Saksham Aggarwal <56749406+Saksham-Aggarwal-1@users.noreply.github.com>
2022-05-31 01:28:51 +0530

    Time: 174 ms (46.39%), Space: 61.3 MB (61.38%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
+    void printarr(vector<int> nums)
+    {
+        for(int i: nums)
+            cout<<i<<" ";
+        cout<<"\n";
+    }
+    
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<nums.size()-1;i++)
+        for(int i=0;i<nums.size();i++)
         {
-            if(nums[i] == nums[i+1])
-                return nums[i];
+            if(nums[abs(nums[i])-1] < 0)
+                return abs(nums[i]);
+            else
+                nums[abs(nums[i])-1] *= -1;
+            //printarr(nums);
         }
-        return -1;
+        
+        return 0;
     }
 };
